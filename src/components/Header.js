6745c0933df4cd7185031ef34c7c70f39bb20d4e
Ftwhh <109472232+Ftwhh@users.noreply.github.com>
2025-05-29 12:53:32 +0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header id="home" className="header">
       <h1 className="header-title">
-        <span className="nrg">NRG</span> Esports
+        <span className="EF">NRG</span> Esports
       </h1>
       <p className="header-description">
         We’re here to create something special
